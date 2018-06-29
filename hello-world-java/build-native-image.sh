621da3a1933790e3080d548a0842c76842bd4d32
Graeme Rocher <graeme.rocher@gmail.com>
2018-06-29 12:30:46 +0200

    Add task to generate reflect json

---
+++ hello-world-java/build-native-image.sh
@@ -0,0 +1,1 @@
+native-image --class-path build/libs/hello-world-java-0.1-all.jar -H:ReflectionConfigurationFiles=reflect.json -H:EnableURLProtocols=http -H:IncludeResources=META-INF/services/*.* -H:Name=hw -H:Class=hello.world.java.Application -H:+ReportUnsupportedElementsAtRuntime
