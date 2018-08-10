929ef90e8c96137116c9c47d9efb39abe457faf5
Graeme Rocher <graeme.rocher@gmail.com>
2018-08-10 11:38:56 +0200

    Latest changes

--- hello-world-java/build-native-image.sh
+++ hello-world-java/build-native-image.sh
@@ -1,1 +1,1 @@
-$GRAAL_HOME/bin/native-image --class-path build/libs/hello-world-java-0.1-all.jar -H:ReflectionConfigurationFiles=reflect.json -H:EnableURLProtocols=http -H:IncludeResources=META-INF/services/*.* -H:Name=hw -H:Class=hello.world.java.Application -H:+ReportUnsupportedElementsAtRuntime
+native-image --class-path build/libs/hello-world-java-0.1-all.jar -H:ReflectionConfigurationFiles=reflect.json -H:EnableURLProtocols=http -H:IncludeResources=META-INF/services/*.* -H:Name=hw -H:Class=hello.world.java.Application -H:+ReportUnsupportedElementsAtRuntime
