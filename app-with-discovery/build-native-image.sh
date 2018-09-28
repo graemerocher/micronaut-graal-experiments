5dd0043eebea9c2412550f8731a42ffb79b0a400
Graeme Rocher <graeme.rocher@gmail.com>
2018-09-28 13:33:52 +0200

    Add discovery-client app

---
+++ app-with-discovery/build-native-image.sh
@@ -0,0 +1,11 @@
+./gradlew assemble
+java -cp build/libs/app-with-discovery-0.1-all.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer 
+native-image --class-path build/libs/app-with-discovery-0.1-all.jar \
+			 -H:ReflectionConfigurationFiles=build/reflect.json \
+			 -H:EnableURLProtocols=http \
+			 -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
+			 -H:Name=app-with-discovery \
+			 -H:Class=app.with.discovery.Application \
+			 -H:+ReportUnsupportedElementsAtRuntime \
+			 -H:+AllowVMInspection \
+			 --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder
