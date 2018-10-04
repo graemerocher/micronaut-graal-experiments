a0a1a76d6d39d2b2928a1ef954854ba053bf989a
Graeme Rocher <graeme.rocher@gmail.com>
2018-10-04 07:26:36 +0200

    Latest changes

--- hello-world-java/build-native-image.sh
+++ hello-world-java/build-native-image.sh
@@ -1,9 +1,11 @@
+./gradlew assemble
+java -cp build/libs/hello-world-java-0.1-all.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer 
 native-image --class-path build/libs/hello-world-java-0.1-all.jar \
 			 -H:ReflectionConfigurationFiles=build/reflect.json \
 			 -H:EnableURLProtocols=http \
 			 -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
-			 -H:Name=hw \
+			 -H:Name=hello-world-java \
 			 -H:Class=hello.world.java.Application \
 			 -H:+ReportUnsupportedElementsAtRuntime \
 			 -H:+AllowVMInspection \
-			 --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder
+			 --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.codec.http.HttpObjectDecoder
