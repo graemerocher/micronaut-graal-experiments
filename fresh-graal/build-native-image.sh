b3a739a032bc7c93901a8c968fb3a690e0dc9bb8
graemerocher <graeme.rocher@gmail.com>
2018-12-18 17:06:08 +0100

    Fix Multiple possible bean candidates error

--- fresh-graal/build-native-image.sh
+++ fresh-graal/build-native-image.sh
@@ -3,12 +3,10 @@ native-image --no-server \
              --allow-incomplete-classpath -cp build/libs/fresh-graal-0.1-all.jar \
              -H:ReflectionConfigurationFiles=reflect.json \
              -H:EnableURLProtocols=http \
-             -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
+             -H:IncludeResources="logback.xml|application.yml" \
              -H:Name=fresh-graal \
              -H:Class=fresh.graal.Application \
              -H:+ReportUnsupportedElementsAtRuntime \
              -H:+AllowVMInspection \
              --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
              --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient
-             
-             # -H:-UseServiceLoaderFeature \
